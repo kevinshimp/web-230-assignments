e1f0e5a748fb1c000346512547318726cb8baed7
kevinshimp <ks95307@ravens.racc.edu>
2018-09-12 13:13:26 -0400

    added swtich folder and index file. Added the switch function.

---
+++ series/switch/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$day = 'Saturday'  ;
+
+switch ($day) {
+  case 'Saturday':
+  case 'Sunday':
+      echo 'It\'s a weekend.';
+    break;
+
+  default:
+      echo 'Not a weekend.';
+    break;
+}
+
+ ?>
