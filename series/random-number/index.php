10a4bcb736433bbb31dd03081a8103265d41a5cb
kevinshimp <ks95307@ravens.racc.edu>
2018-10-28 21:50:56 -0400

    60

--- series/random-number/index.php
+++ series/random-number/index.php
@@ -9,3 +9,14 @@ if (isset($_POST['roll'])) {
 <form action = "index.php" method="POST">
     <input type="submit" name= "roll" value= "Roll dice.">
 </form>
+
+<!--
+ 01. Explaine what the if statment is doing here. if(isset($_POST['roll'])){
+Checking if the button has been clicked and telling it to roll if it has been.
+
+ 02. Explaine what the rand function is. $rand = rand(1, 6);
+it is picking a random number between 1 and 6
+
+ 03. Explain a way the $rand might be used in a web application.
+Games of chance, ways to give a random user id, ways to generate passwords or captchas.
+   -->
