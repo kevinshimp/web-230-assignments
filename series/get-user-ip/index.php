4cdc965af724fba719756a3471c2e4f577940d6e
kevinshimp <ks95307@ravens.racc.edu>
2018-10-28 22:56:13 -0400

    65

--- series/get-user-ip/index.php
+++ series/get-user-ip/index.php
@@ -11,3 +11,12 @@ foreach($ip_blocked as $ip) {
  ?>
 
 <h1>Welcome!</h1>
+
+<!--
+// 01. Explain what this for each loop is doing. foreach ($ip_blocked as $ip) { if($ip == $ip_address){
+die('Your IP address, '.$ip_address.' has been blocked'); } }
+
+It is essentially checking each item in the array to see which ip addresses in teh array are blocked.
+
+
+-->
